1c5d98aa7983d8a5e6f365c1a7d6ec7d6d29dc60
XDH <xdh@XDHdeMacBook-Air.local>
2020-02-08 22:21:19 +0800

    项目起步,路由整合

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+//导入vue
+import Vue from 'vue'
+//导入Vue-router
+import VueRouter from 'vue-router'
+// 注册一下 use
+Vue.use(VueRouter)
+// 创建路由对象
+const router = new VueRouter({
+    // 路由规则
+    routers:[
+
+    ]
+})
+// 暴露出去
+export default router
